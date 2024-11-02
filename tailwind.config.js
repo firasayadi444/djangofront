c1d19de58acd3bc81478d3b15aadbd7173f2cd8a
wajdi19 <wajdibejaoui50@gmail.com>
2024-11-02 14:38:49 +0100

    missing files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+import withMT from "@material-tailwind/html/utils/withMT";
+/** @type {import('tailwindcss').Config} */
+module.exports = withMT({
+  content: [
+    './src/**/*.{html,ts}',
+    './src/**/**/*.{html,ts}',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
+
